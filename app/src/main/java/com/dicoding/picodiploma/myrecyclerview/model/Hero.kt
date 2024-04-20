9003405a35de81fac5f2a8bb4e5f5234ad210f43
er4files <rahmaderasugiarto@gmail.com>
2024-04-21 00:09:17 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/myrecyclerview/model/Hero.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.myrecyclerview.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
